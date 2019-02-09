4f5a43dae81f5522e0103d30019ab5c71d688796
Ashish Rastogi <ashish.rastogi18@gmail.com>
2019-02-08 22:29:15 -0500

    Added CI/CD

--- gulpfile.js
+++ gulpfile.js
@@ -2,5 +2,14 @@
 
 const gulp = require('gulp');
 const build = require('@microsoft/sp-build-web');
+//added for CI/CD
+require('./gulpfile-deploy-app-package');
+require('./gulpfile-serve-info');
+require('./gulpfile-update-manifest');
+require('./gulpfile-upload-to-sharepoint');
+require('./gulpfile-upload-app-package');
 
 build.initialize(gulp);
+
+
+
